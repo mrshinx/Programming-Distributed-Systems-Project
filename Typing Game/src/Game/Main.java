0f69970f2f19e75eb53149f168e44eb5596407f9
monishsuresh <58329690+monishsuresh@users.noreply.github.com>
2020-11-19 14:10:24 +0400

    Update Main.java
    Added Methods:
    Line 217: CheckForDuplicate Method- checks if account name already exists
    Line 233: GetUsernameAndPassword Method- its the same piece of code from the Login() method that gets account details
    
    Modified:
    Line 60: 'if(password.length() >0)' is now under 'if(account.length() >0)'. And also CheckForDuplicate is called here 
    Line 94: replaced previous code with GetUsernameAndPassword Method

--- Typing Game/src/Game/Main.java
+++ Typing Game/src/Game/Main.java
@@ -3,11 +3,11 @@ import java.io.*;
 import java.util.*;
 public class Main {
     @SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         ClearScreen();
         int input =-1;
         Scanner inputCommand = new Scanner(System.in);
-        while(input <0) 
+        while(input <0)
         {
             try {
                 System.out.println("Press 1 to register, press 2 to login: ");
@@ -56,18 +56,24 @@ public class Main {
 
         System.out.println("Please enter account: ");
         account = sc.nextLine();
+
         if(account.length() >0)
         {
-            bw.write(account);
-        }
+            if(!CheckForDuplicate(filePath, account)){
+                bw.write(account);
 
-        System.out.println("Please enter password: ");
-        password = sc.nextLine();
-        if(password.length() >0)
-        {
-            bw.write(" ");
-            bw.write(password);
-            bw.newLine();
+                System.out.println("Please enter password: ");
+                password = sc.nextLine();
+                if(password.length() >0)
+                {
+                    bw.write(" ");
+                    bw.write(password);
+                    bw.newLine();
+                }
+            }
+            else{
+                System.out.println("Account name already exists, try another one");
+            }
         }
 
         bw.close();
@@ -77,34 +83,15 @@ public class Main {
     public static void Login() throws IOException
     {
         String textLine, account, password , choice ,newpass ,  filePath= "Account Database.txt";
-  
+
         int count =0;
         int index =0;
-        String[] accountInfo;
+
         ArrayList<String> accountList = new ArrayList<String>();
         ArrayList<String> passwordList = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
 
-        BufferedReader br = null;
-        try
-        {
-            br = new BufferedReader(new FileReader(filePath));
-        }
-        catch(Exception e)
-        {
-            System.out.println("Cannot open file, program terminated");
-            main(null);
-        }
-
-        textLine = br.readLine();
-        while(textLine!=null)
-        {
-            accountInfo = textLine.split(" ");
-            accountList.add(accountInfo[0]);
-            passwordList.add(accountInfo[1]);
-            textLine = br.readLine();
-        }
-        br.close();
+        GetUsernameAndPassword(filePath, accountList, passwordList);
 
         System.out.println("Enter your account: " );
         account = scanner.nextLine();
@@ -124,9 +111,9 @@ public class Main {
         }
 
         System.out.println("Enter your password: " );
-    
-       password= scanner.nextLine();
-        
+
+        password= scanner.nextLine();
+
         if (password.equals(passwordList.get(index)))
         {
             System.out.println("Logged in successfully!");
@@ -142,32 +129,32 @@ public class Main {
                     System.out.println("Enter your new password");
                     newpass= scanner.nextLine();
                     String oldContent = "" ;
-                    File fileToBeModified = new File(filePath); 
+                    File fileToBeModified = new File(filePath);
                     BufferedReader reader = null;
-                    FileWriter writer = null;   
+                    FileWriter writer = null;
                     try
                     {
                         reader = new BufferedReader(new FileReader(fileToBeModified));
-                         
+
                         //Reading all the lines of input text file into oldContent
-                         
+
                         String line = reader.readLine();
-                         
-                        while (line != null) 
+
+                        while (line != null)
                         {
                             oldContent = oldContent + line + System.lineSeparator();
-                             
+
                             line = reader.readLine();
                         }
-                         
+
                         //Replacing oldString with newString in the oldContent
-                         
+
                         String newContent = oldContent.replaceAll(password, newpass);
-                         
+
                         //Rewriting the input text file with newContent
-                         
+
                         writer = new FileWriter(fileToBeModified);
-                         
+
                         writer.write(newContent);
                         System.out.println("Password successfully changed");
                     }
@@ -180,56 +167,96 @@ public class Main {
                         try
                         {
                             //Closing the resources
-                             
+
                             reader.close();
-                             
+
                             writer.close();
-                        } 
-                        catch (IOException e) 
+                        }
+                        catch (IOException e)
                         {
                             e.printStackTrace();
                         }
                     }
-                break;
-                 
+                    break;
+
                 default:
-                	System.out.println("Invalid input");
-                	Login();
+                    System.out.println("Invalid input");
+                    Login();
             }
 
-            	
-           
+
+
         }
 
         else {
-        System.out.println("Invalid password.");
+            System.out.println("Invalid password.");
         }
         main(null);
     }
-    
-  
-            /*	System.out.println("Enter your new password");
-            	newpass = sc.nextLine();
-            	Path path = Paths.get("Account Database.txt");
-            	Charset charset = StandardCharsets.UTF_8;
 
-            	String content = new String(Files.readAllBytes(path), charset);
-            	content = content.replaceAll(old, newpass);
-            	Files.write(path, content.getBytes(charset));
-            	System.out.println("Successfully changed"); */
-public static void play() throws IOException
-{ System.out.println("In progress..");
- main(null);
-}
-    
+
+    /*	System.out.println("Enter your new password");
+        newpass = sc.nextLine();
+        Path path = Paths.get("Account Database.txt");
+        Charset charset = StandardCharsets.UTF_8;
+        String content = new String(Files.readAllBytes(path), charset);
+        content = content.replaceAll(old, newpass);
+        Files.write(path, content.getBytes(charset));
+        System.out.println("Successfully changed"); */
+    public static void play() throws IOException
+    { System.out.println("In progress..");
+        main(null);
+    }
+
 
     public static void ClearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
+    public static Boolean CheckForDuplicate(String filePath, String accountname) throws IOException{
+
+        ArrayList<String> accountList = new ArrayList<String>();
+        ArrayList<String> passwordList = new ArrayList<String>();
+        GetUsernameAndPassword(filePath, accountList, passwordList);
+
+        if(accountList.contains(accountname)){
+            return  true;
+        }
+
+        else{
+            return  false;
+        }
+    }
+
+
+    public static void GetUsernameAndPassword(String filePath, ArrayList<String> accountList,
+                                                     ArrayList<String> passwordList) throws IOException{
+
+        String[] accountInfo;
+
+        BufferedReader br = null;
+        String textLine;
+        try
+        {
+            br = new BufferedReader(new FileReader(filePath));
+        }
+        catch(Exception e)
+        {
+            System.out.println("Cannot open file, program terminated");
+            main(null);
+        }
+
+        textLine = br.readLine();
+        while(textLine!=null)
+        {
+            accountInfo = textLine.split(" ");
+            accountList.add(accountInfo[0]);
+            passwordList.add(accountInfo[1]);
+
+            textLine = br.readLine();
+        }
+        br.close();
+    }
+
 }
-
-
-
-
